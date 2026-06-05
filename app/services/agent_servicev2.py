bf073332bd86ad8ccebb5951025ff45b3e15264b
Randy <alejobarboza54@gmail.com>
2026-06-05 15:40:09 -0500

    Mejora de Moldelo de agente

--- app/services/agent_servicev2.py
+++ app/services/agent_servicev2.py
@@ -11,7 +11,7 @@ from langchain_core.runnables.history import RunnableWithMessageHistory
 load_dotenv()
 
 llm = ChatOpenAI(
-    model="",
+    model="openai/gpt-4o-mini",
     openai_api_key=os.getenv("OPENROUTER_API_KEY"),
     openai_api_base="https://openrouter.ai/api/v1"
 )
